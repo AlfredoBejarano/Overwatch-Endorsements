97cf3edade75704de5af1f28d4d30120f824598e
AlfredoBejarano <alfredocbejarano@gmail.com>
2018-07-26 01:20:48 -0500

    Adds launcher activiy

---
+++ app/src/main/java/com/alfredobejarano/endorsements/EndorsementsActivity.kt
@@ -0,0 +1,12 @@
+package com.alfredobejarano.endorsements
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EndorsementsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_endorsements)
+    }
+}
