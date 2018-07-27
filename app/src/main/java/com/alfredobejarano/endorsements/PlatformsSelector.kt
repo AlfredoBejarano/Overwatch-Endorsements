2669ba018b4ad7d1ee579af955c777febf189002
AlfredoBejarano <alfredocbejarano@gmail.com>
2018-07-27 14:26:16 -0500

    Adds platform selector view

---
+++ app/src/main/java/com/alfredobejarano/endorsements/PlatformsSelector.kt
@@ -0,0 +1,40 @@
+package com.alfredobejarano.endorsements
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.CheckedTextView
+import android.widget.LinearLayout
+import com.alfredobejarano.endorsements.repository.remote.Platforms
+import kotlinx.android.synthetic.main.view_platform_selector.view.*
+
+class PlatformsSelector(context: Context?, attrs: AttributeSet?) : LinearLayout(context, attrs) {
+    var selectedPlatform = Platforms.PC
+    private val root: View = LayoutInflater.from(context).inflate(R.layout.view_platform_selector, this, true)
+    private val selector: LinearLayout = root.selector
+
+    init {
+        for (i in 0 until selector.childCount) {
+            selector.getChildAt(i).setOnClickListener {
+                it.setOnClickListener { selectPlatforms(it.id) }
+            }
+        }
+    }
+
+    private fun selectPlatforms(viewId: Int) {
+        for (i in 0 until selector.childCount) {
+            val child = root.selector.getChildAt(i) as CheckedTextView
+            if (child.id == viewId) {
+                child.isChecked = true
+                selectedPlatform = when (viewId) {
+                    R.id.pc -> Platforms.PC
+                    R.id.xbl -> Platforms.XBL
+                    else -> Platforms.PSN
+                }
+            } else {
+                child.isChecked = false
+            }
+        }
+    }
+}
