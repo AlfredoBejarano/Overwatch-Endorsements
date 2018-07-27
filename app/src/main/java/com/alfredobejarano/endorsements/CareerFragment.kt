791456423cd2cb5ddd3fc529274895617b3f1946
AlfredoBejarano <alfredocbejarano@gmail.com>
2018-07-27 13:46:18 -0500

    Creates CareerFragment.kt

---
+++ app/src/main/java/com/alfredobejarano/endorsements/CareerFragment.kt
@@ -0,0 +1,43 @@
+package com.alfredobejarano.endorsements
+
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import com.alfredobejarano.endorsements.repository.remote.Platforms
+import com.alfredobejarano.endorsements.viewmodel.ProfileViewModel
+import kotlinx.android.synthetic.main.fragment_career.*
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class CareerFragment : Fragment() {
+    /**
+     * Create this fragment content view.
+     * @param inflater The defined layout inflater for this fragment.
+     * @param container The layout that is containing this fragment.
+     */
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_career, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val careerVM = ViewModelProviders.of(activity as FragmentActivity)[ProfileViewModel::class.java]
+        battletag?.setOnEditorActionListener { v, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_DONE) {
+                careerVM.getProfileData(Platforms.PC, (v as EditText?)?.text)
+                return@setOnEditorActionListener true
+            } else {
+                return@setOnEditorActionListener false
+            }
+        }
+    }
+}
