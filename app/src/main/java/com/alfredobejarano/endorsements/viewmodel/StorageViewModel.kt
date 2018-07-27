06e4bd9c789af28eba99891fec79b7d0e8aa61e8
AlfredoBejarano <alfredocbejarano@gmail.com>
2018-07-27 13:45:23 -0500

    Adds ViewModel for local database access

---
+++ app/src/main/java/com/alfredobejarano/endorsements/viewmodel/StorageViewModel.kt
@@ -0,0 +1,28 @@
+package com.alfredobejarano.endorsements.viewmodel
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.MutableLiveData
+import com.alfredobejarano.endorsements.model.PlayerProfile
+import com.alfredobejarano.endorsements.repository.local.AppDatabase
+import kotlin.concurrent.thread
+
+class StorageViewModel(application: Application) : AndroidViewModel(application) {
+    /**
+     * MutableLiveData property for retrieving a session.
+     */
+    val session: MutableLiveData<PlayerProfile> = MutableLiveData()
+
+    /**
+     * Retrieve the current local session.
+     */
+    fun getStoredProfile() = thread(start = true, name = "${this::class.java} thread") {
+        // Retrieve a stored session from the database.
+        val localSession = AppDatabase
+                .getInstance(getApplication())
+                .playerProfileDao()
+                .getPlayerProfile()
+        // Set the retrieved session as the value of the session property.
+        session.postValue(localSession)
+    }
+}
